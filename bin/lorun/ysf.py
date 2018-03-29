111d0d2d771d01c1bc3baf14a1f57a8b5327905f
yangshunfeng <972536780@qq.com>
2018-03-29 22:12:44 +0800

    信息

--- bin/lorun/ysf.py
+++ bin/lorun/ysf.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
             jsonStr = json.dumps(data)
             print jsonStr
         except Exception, e:
-            jsonStr = e.message
+            jsonStr = e
             print e
             pass
         finally:
